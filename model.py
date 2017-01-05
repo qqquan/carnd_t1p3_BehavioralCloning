d0adcf346c11cb6711e0e2d75d69ca18ee345981
Qs <quan.s.box@gmail.com>
2017-01-04 21:28:52 -0500

    add one more maxpool to reduce hpyer-param numbers for nvida model

--- model.py
+++ model.py
@@ -68,8 +68,9 @@ class qModelTrainer:
 
 
         self.model.add(BatchNormalization())
-        self.model.add(Convolution2D(48, 5, 5, subsample=(2, 2), name='cnn2', border_mode='valid', ) )
+        self.model.add(Convolution2D(48, 5, 5, name='cnn2', border_mode='valid', ) )
         self.model.add(ELU())
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
 
 
         self.model.add(BatchNormalization())
