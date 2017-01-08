34c3ddd1990f9bb2651ed55e0be6b504bb688424
Qs <quan.s.box@gmail.com>
2017-01-07 22:19:52 -0500

    add rec11_backwardTrack with udacity data and rec13_sideDirt1

--- model.py
+++ model.py
@@ -30,7 +30,8 @@ class qModelTrainer:
         else:
              ls_records = [  
                             'recordings/rec13_sideDirt1/driving_log.csv',
-                            'recordings/rec14_backTrack3/driving_log.csv',
+                            'recordings/rec11_backwardTrack/driving_log.csv',
+                            # 'recordings/rec14_backTrack3/driving_log.csv',
                             # 'recordings/rec10_right_turn/driving_log.csv',
                             # 'recordings/rec3_finer_steering/driving_log.csv',
                             # 'recordings/rec2_curve/driving_log.csv',
