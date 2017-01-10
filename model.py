31c8348f43f02d7da6256981f4f106dda5a0e71c
Qs <quan.s.box@gmail.com>
2017-01-09 22:29:49 -0500

    add one more Dropout, because it seems overfitting faster

--- model.py
+++ model.py
@@ -144,6 +144,7 @@ class qModelTrainer:
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         
         self.model.add(ELU())
+        self.model.add(Dropout(.5))
 
         self.model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
         self.model.add(ELU())
