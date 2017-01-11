490ff247fa9884ef5cf097bf6b6db31e709f65af
Qs <quan.s.box@gmail.com>
2017-01-10 22:05:24 -0500

    add more recording on the side-dirt road

--- model.py
+++ model.py
@@ -33,6 +33,7 @@ class qModelTrainer:
              ls_records = [  
                             'recordings/rec16_troubleSpots/driving_log.csv',
                             'recordings/rec18_rightTurn/driving_log.csv',
+                            'recordings/rec17_troubl_dirt/driving_log.csv',
                             # 'recordings/rec15_MentorSD/driving_log.csv',
                             # 'recordings/rec13_sideDirt1/driving_log.csv',
                             # 'recordings/rec11_backwardTrack/driving_log.csv',
